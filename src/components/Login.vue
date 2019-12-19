9b2fdf560c0c624b57058aa035792aef8800ba37
mlbxly <1031468560@qq.com>
2019-12-19 17:22:56 +0800

    login页面修改

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -38,8 +38,8 @@
     <div class="from-wrap">
       <h2>登录</h2>
       <Form ref="loginData" :model="loginData" :rules="ruleValidate" :label-width="80">
-        <FormItem label="账号" prop="username">
-          <Input type="text" v-model="loginData.username" placeholder="请输入账号"></Input>
+        <FormItem label="账号/手机号码" prop="phone">
+          <Input type="text" v-model="loginData.phone" placeholder="请输入账号/手机号码"></Input>
         </FormItem>
         <FormItem label="密码" prop="password">
           <Input type="password" v-model="loginData.password" placeholder="请输入密码"></Input>
@@ -57,11 +57,11 @@
     data () {
       return {
         loginData: {
-          username:'',
+          phone:'',
           password:''
         },
         ruleValidate: {
-          username: [
+          phone: [
             { required: true, message: '账号不能为空', trigger: 'blur' },
             { min: 3, max: 15, message: '账号长度3-16个字符', trigger: 'blur' }
           ],
